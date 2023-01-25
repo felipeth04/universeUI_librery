268edd82662e121edc32864220b788db795c33f4
felipeth04 <felipeth4@hotmail.com>
2023-01-25 17:48:52 -0500

    add settings to control the table data on storybook

--- stories/atoms/Button/Button.stories.js
+++ stories/atoms/Button/Button.stories.js
@@ -3,13 +3,49 @@ import {options} from "./constants";
 
 export default  {
     title: "Button",
-    component: Button
+    component: Button,
+    args: {
+        text: "Hello world"
+    },
+    argTypes: {
+        text:{
+            description: "**Options**",
+            control: {
+                type: "textInputs", options: "hello world"
+            }
+        },
+        color: {
+            description: "**Options**",
+            table: {
+                type: {
+                    summary: options.colors.map(option => `'${option}'`).join("|")
+                }
+            },
+            control: {
+                type: "select", options: options.colors
+            }
+        },
+        size: {
+            description: "**Options**",
+            table: {
+                type: {
+                    summary: options.sizes.map(option => `'${option}'`).join("|")
+                }
+            },
+            control: {
+                type: "select", options: options.sizes
+            }
+        }
+    }
 }
 
 const Template = (args) => <Button {...args}/>
-const listTemplate = ({items, ...args}) => items.map((item,index) => <Button key={index} {...args} {...item}/>)
 
-export const Default = () => Template.bind({})
+const listTemplate = ({items, ...args}) =>
+                        items.map((item,index) =>
+                            <Button key={index} {...args} {...item}/>)
+
+export const Default = Template.bind()
 
 export const Colors = listTemplate.bind({})
 Colors.args = {
